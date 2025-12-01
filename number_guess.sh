139873698544cf568ce46dbd860b9db7b0fb0674
PK <2023412heng@aupp.edu.kh>
2025-12-01 07:12:48 -0500

    feat: add user database integration

--- number_guess.sh
+++ number_guess.sh
@@ -5,3 +5,19 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 # Prompt for username
 echo "Enter your username:"
 read USERNAME
+
+# Check if user exists
+USER_INFO=$($PSQL "SELECT user_id, games_played, best_game FROM users WHERE username='$USERNAME'")
+
+if [[ -z $USER_INFO ]]
+then
+  # New user
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  # Insert new user
+  INSERT_USER=$($PSQL "INSERT INTO users(username, games_played, best_game) VALUES('$USERNAME', 0, 0)")
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+else
+  # Returning user
+  IFS="|" read USER_ID GAMES_PLAYED BEST_GAME <<< "$USER_INFO"
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
