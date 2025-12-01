7e08db634eee4fe112ea0e9a8797ce092b4ba7a8
PK <2023412heng@aupp.edu.kh>
2025-12-01 06:49:12 -0500

    feat: implement guessing logic with higher/lower hints

--- number_guess.sh
+++ number_guess.sh
@@ -2,6 +2,9 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
+# Generate random number between 1 and 1000
+SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+
 # Prompt for username
 echo "Enter your username:"
 read USERNAME
@@ -21,3 +24,25 @@ else
   IFS="|" read USER_ID GAMES_PLAYED BEST_GAME <<< "$USER_INFO"
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
+
+# Start guessing game
+echo "Guess the secret number between 1 and 1000:"
+NUMBER_OF_GUESSES=0
+
+while true
+do
+  read GUESS
+  NUMBER_OF_GUESSES=$(( NUMBER_OF_GUESSES + 1 ))
+  
+  # Check if guess is correct
+  if [[ $GUESS -eq $SECRET_NUMBER ]]
+  then
+    echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+    break
+  elif [[ $GUESS -gt $SECRET_NUMBER ]]
+  then
+    echo "It's lower than that, guess again:"
+  else
+    echo "It's higher than that, guess again:"
+  fi
+done
